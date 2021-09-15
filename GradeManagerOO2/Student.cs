ef378783b36ee34bdd4d12a0a9bacaff21cf9bdc
robert mix <eurotuner1981@gmail.com>
2021-09-15 17:50:11 +0200

    Started adding to student class with new ideas

--- GradeManagerOO2/Student.cs
+++ GradeManagerOO2/Student.cs
@@ -8,18 +8,35 @@ namespace Grade_Manager_OO
 {
     public class Student
     {
-        public string studentName { get; set; } = "Not defined";
-        public double average { get; set; } = 0.0;
-        public bool completionStatus { get; set; } = false;
+        public string StudentName { get; set; }
+        public double Average { get; set; } = 0.0;
+        public bool AllAssignmentsCompleteTrueOrFalse { get; set; } = false;
+
+        public Dictionary<string, Assignment> assignments = new Dictionary<string, Assignment>();
+
+        public Student(string StudentName, double studentAverage, bool allAssignmentsComplete)
+        {
+            this.StudentName = StudentName;
+            this.Average = studentAverage;
+            this.AllAssignmentsCompleteTrueOrFalse = allAssignmentsComplete;
+        }
 
         
 
-        public Student(string StudentName, double studentAverage, bool studentCompletionStatus)
-        {
-            this.studentName = StudentName;
-            this.average = studentAverage;
-            this.completionStatus = studentCompletionStatus;
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         public static void ShowStudentSummary()
         {
@@ -36,21 +53,17 @@ namespace Grade_Manager_OO
 
 
         }
-
         public static void ShowAssignments()
         {
             Console.WriteLine("Show assignments");
         }
-
         public static void ShowStudentBestGrade()
         {
             Console.WriteLine("Show student best grade");
         }
-
         public static void ShowStudentWorstGrade()
         {
             Console.WriteLine("Show worst grade");
-
         }
 
     }
