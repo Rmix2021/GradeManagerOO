222312fc5d79d8f6d67dbbd81214d851ebc8074c
robert mix <eurotuner1981@gmail.com>
2021-09-16 01:15:59 +0200

    Student Class is almost ready to test

--- GradeManagerOO2/ClassRoom.cs
+++ GradeManagerOO2/ClassRoom.cs
@@ -6,62 +6,137 @@ using System.Threading.Tasks;
 
 namespace Grade_Manager_OO
 {
-    class ClassRoom
+    public class ClassRoom
     {
         public string name { get; set; } = "Default class room";
 
-        public Dictionary<string, ClassRoom> _classRooms = new Dictionary<string, ClassRoom>();
+        public Dictionary<string, Student> Student = new Dictionary<string, Student>();
 
         public ClassRoom(string classRoomName)
         {
             this.name = classRoomName;
         }
 
-        public static void ShowStudents()
+        public void ClassRoomDetailsMenu()
         {
-            Console.WriteLine("Students");
+            Console.Clear();
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "*********Edit Classrooms Menu**********"));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "Currently Editing 'classroom xyz'      "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "1. Show Students.                      "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "2. Add Student.                        "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "3. Remove Student.                     "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "4. Student Details Menu.               "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "5. Show class average.                 "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "6. Show top student.                   "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "7. Show worst student.                 "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "8. Compare 2 students.                 "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "9. Exit this menu.                     "));
+            Console.SetCursorPosition(21, 12);
+
+            string classRoomChoice = Console.ReadLine();
+
+            switch (classRoomChoice)
+            {
+                case "1":
+                    ShowStudents();
+                    {
+                        break;
+                    }
+
+                case "2":
+                    AddStudent();
+                    {
+                        break;
+                    }
+
+                case "3":
+                    RemoveStudent();
+                    {
+                        break;
+                    }
+
+                case "4":
+                    //Student[StudentName].EditStudentDetailsMenu();
+                    {
+                        break;
+                    }
+
+                case "5":
+                    ShowClassAverage();
+                    {
+                        break;
+                    }
+
+                case "6":
+                    ShowTopStudent();
+                    {
+                        break;
+                    }
+
+                case "7":
+                    ShowBottomStudent();
+                    {
+                        break;
+                    }
+
+                case "8":
+                    Compare2Student();
+                    {
+                        break;
+                    }
+                case "9":
+                    ExitThisMenu();
+                    {
+                        break;
+                    }
+            }
+
+            void ShowStudents()
+            {
+                Console.WriteLine("Students");
+            }
+
+            void AddStudent()
+            {
+                Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "Name the Student in which you would like to add."));
+                Console.SetCursorPosition(21, 9);
+                string studentName = Console.ReadLine();
+
+
+            }
+
+            void RemoveStudent()
+            {
+                Console.WriteLine(" Remove Student");
+            }           
+
+            void ShowClassAverage()
+            {
+                Console.WriteLine("Show average");
+            }
+
+            void ShowTopStudent()
+            {
+                Console.WriteLine("Show top student");
+            }
+
+            void ShowBottomStudent()
+            {
+                Console.WriteLine("Show bottom student");
+            }
+
+            void Compare2Student()
+            {
+                Console.WriteLine("Compare 2 Students");
+            }
+
+            void ExitThisMenu()
+            {
+                Console.Clear();
+                MainMenu.Menu();
+            }
+
         }
-
-        public static void AddStudent()
-        {
-            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "Name the Student in which you would like to add."));
-            Console.SetCursorPosition(21, 9);
-            string studentName = Console.ReadLine();
-
-        }
-
-        public static void RemoveStudent()
-        {
-            Console.WriteLine(" Remove Student");
-        }
-
-        public static void EditStudentDetailsMenu()
-        {
-            Console.WriteLine("EDit student menu");
-        }
-
-        public static void ShowClassAverage()
-        {
-            Console.WriteLine("Show average");
-        }
-
-        public static void ShowTopStudent()
-        {
-            Console.WriteLine("Show top student");
-        }
-
-        public static void ShowBottomStudent()
-        {
-            Console.WriteLine("Show bottom student");
-        }
-
-        public static void Compare2Student()
-        {
-            Console.WriteLine("Compare 2 Students");
-        }
-
-
-
     }
 }
 
--- GradeManagerOO2/Student.cs
+++ GradeManagerOO2/Student.cs
@@ -12,56 +12,111 @@ namespace Grade_Manager_OO
         public double Average { get; set; } = 0.0;
         public bool AllAssignmentsCompleteTrueOrFalse { get; set; } = false;
 
-        public Dictionary<string, Assignment> assignments = new Dictionary<string, Assignment>();
-
-        public Student(string StudentName, double studentAverage, bool allAssignmentsComplete)
+        public Student(string StudentNamed)
         {
-            this.StudentName = StudentName;
-            this.Average = studentAverage;
-            this.AllAssignmentsCompleteTrueOrFalse = allAssignmentsComplete;
+            this.StudentName = StudentNamed;        
         }
 
-        
+        public Dictionary<string, Assignment> assignmentsDictionary = new Dictionary<string, Assignment>();
 
 
+        public void EditStudentDetailsMenu()
+        {
+            Console.Clear();
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "******Edit Student Details Menu********"));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "Currently Editing 'Student xyz'        "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "1. Show Student Summary.               "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "2. Assign Student New Assignment.      "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "3. Show Assignments.                   "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "4. Grade Assignments.                  "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "5. Show students Best Grade.           "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "6. Show students worst Grade.          "));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "9. Exit this menu.                     "));
+            Console.SetCursorPosition(21, 10);
+
+            string StudentDetailsChoice = Console.ReadLine();
+            switch (StudentDetailsChoice)
+            {
+                case "1":
+                    ShowStudentSummary();
+                    {
+                        break;
+                    }
+
+                case "2":
+                    AssignNewAssignment();
+                    {
+                        break;
+                    }
+
+                case "3":
+                    ShowAssignments();
+                    {
+                        break;
+                    }
+
+                case "4": /*GradeAssignments();*/
+                    {
+                        break;
+                    }
+
+                case "5":
+                    ShowStudentBestGrade();
+                    {
+                        break;
+                    }
+
+                case "6":
+                    ShowStudentWorstGrade();
+                    {
+                        break;
+                    }
 
 
+                case "9":
+                    ExitThisMenu();
+                    {
+                        break;
+                    }
+            }
+
+            void ExitThisMenu()
+            {
+                Console.Clear();
+                MainMenu.Menu();
+            }
+        }
 
 
-
-
-
-
-
-
-
-
-
-
-        public static void ShowStudentSummary()
+        void ShowStudentSummary()
         {
             Console.WriteLine("Show Student details");
         }
 
-        public static void AssignNewAssignment()
+
+        void AssignNewAssignment()
         {
             Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "Name the assignment in which yo would like to add."));
             Console.SetCursorPosition(21, 9);
+            //string assignmentName = Console.ReadLine();
+
             string assignmentName = Console.ReadLine();
-            
-            
-
-
+            assignmentsDictionary.Add(assignmentName, new Assignment(assignmentName));
         }
-        public static void ShowAssignments()
+
+        void ShowAssignments()
         {
             Console.WriteLine("Show assignments");
         }
-        public static void ShowStudentBestGrade()
+
+
+        void ShowStudentBestGrade()
         {
             Console.WriteLine("Show student best grade");
         }
-        public static void ShowStudentWorstGrade()
+
+
+        void ShowStudentWorstGrade()
         {
             Console.WriteLine("Show worst grade");
         }
